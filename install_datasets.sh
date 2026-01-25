723aae2f118b5fd2f4c96951c8a40314f8ca9fdf
Zhi Yong <93578721+zhiyongggggg@users.noreply.github.com>
2026-01-25 14:56:43 +0800

    install dataset checks for directory path first

--- install_datasets.sh
+++ install_datasets.sh
@@ -1,98 +1,134 @@
 #!/usr/bin/env bash
+set -e  # exit immediately on error
 
-helpFunction()
-{
+helpFunction() {
   echo ""
-  echo "Usage: $0 -a parameterA -b parameterB -c parameterC -d parameterD"
-  echo -e "\t-a Download and install ImageNette?     type:(Y/N)"
-  echo -e "\t-b Download and install mini-ImageNet?  type:(Y/N)"
-  echo -e "\t-c Download and install ImageNet?       type:(Y/N)"
-  echo -e "\t-d Directory path for datasets          type:PATH"
-  exit 1 # Exit script after printing help
+  echo "Usage: $0 -a Y/N -b Y/N -c Y/N -d DATASET_DIR"
+  echo -e "\t-a Download ImageNette     (Y/N)"
+  echo -e "\t-b Download mini-ImageNet  (Y/N)"
+  echo -e "\t-c Download ImageNet-1K    (Y/N)"
+  echo -e "\t-d Dataset root directory"
+  exit 1
 }
 
-while getopts "a:b:c:d:" opt
-do
+while getopts "a:b:c:d:" opt; do
   case "$opt" in
-    a ) parameterA="$OPTARG" ;;
-    b ) parameterB="$OPTARG" ;;
-    c ) parameterC="$OPTARG" ;;
-    d ) parameterD="$OPTARG" ;;
-    ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+    a) parameterA="$OPTARG" ;;
+    b) parameterB="$OPTARG" ;;
+    c) parameterC="$OPTARG" ;;
+    d) parameterD="$OPTARG" ;;
+    ?) helpFunction ;;
   esac
 done
 
-# Print helpFunction in case parameters are empty
-if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterC" ] || [ -z "$parameterD" ]
-then
-  echo "Some or all of the parameters are empty";
+if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterC" ] || [ -z "$parameterD" ]; then
+  echo "Missing required arguments"
   helpFunction
 fi
 
-# Set dataset directory
-ROOT_DIR=${pwd}
-DATASET_DIR=${parameterD}
-cd DATASET_DIR
+ROOT_DIR=$(pwd)
+DATASET_DIR="$parameterD"
 
+# Ensure dataset root exists
+mkdir -p "$DATASET_DIR"
+cd "$DATASET_DIR" || { echo "Cannot access $DATASET_DIR"; exit 1; }
+
+echo "Using dataset directory: $DATASET_DIR"
+
+# --------------------------------------------------
 # ImageNette
-if [ "${parameterA}" == Y ]; then
-  echo -e "\n"
-  echo "Downloading and installing ImageNette..."
-  mkdir ImageNette
+# --------------------------------------------------
+if [ "$parameterA" == "Y" ]; then
+  echo "ImageNette"
+
+  mkdir -p ImageNette
   cd ImageNette
-  wget https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz
-  tar -zxvf imagenette2.tgz
-  cd DATASET_DIR
+
+  if [ ! -f imagenette2.tgz ]; then
+    wget https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz
+  else
+    echo "imagenette2.tgz already exists, skipping download"
+  fi
+
+  if [ ! -d imagenette2 ]; then
+    tar -zxvf imagenette2.tgz
+  else
+    echo "ImageNette already extracted"
+  fi
+
+  cd "$DATASET_DIR"
 fi
 
-# miniImageNet
-if [ "${parameterB}" == Y ]; then
-  echo -e "\n"
-  echo "Downloading and installing miniImageNet..."
-  mkdir miniIamgeNet
-  cd miniImagenet
-  wget https://github.com/twitter/meta-learning-lstm/blob/master/data/miniImagenet/test.csv
-  wget https://github.com/twitter/meta-learning-lstm/blob/master/data/miniImagenet/train.csv
-  wget https://github.com/twitter/meta-learning-lstm/blob/master/data/miniImagenet/val.csv
-  wget http://image-net.org/image/ILSVRC2015/ILSVRC2015_CLS-LOC.tar.gz
-  tar -zxvf ILSVRC2015_CLS-LOC.tar.gz
+# --------------------------------------------------
+# mini-ImageNet
+# --------------------------------------------------
+if [ "$parameterB" == "Y" ]; then
+  echo "mini-ImageNet"
 
-  cd ROOT_DIR
-  python compressed_cryptonetes/data/make_miniImagenet_json.py --dataset_dir DATASET_DIR
-  cd DATASET_DIR
+  mkdir -p miniImageNet
+  cd miniImageNet
+
+  for file in train.csv val.csv test.csv; do
+    if [ ! -f "$file" ]; then
+      wget https://github.com/twitter/meta-learning-lstm/raw/master/data/miniImagenet/$file
+    else
+      echo "$file already exists"
+    fi
+  done
+
+  if [ ! -f ILSVRC2015_CLS-LOC.tar.gz ]; then
+    wget http://image-net.org/image/ILSVRC2015/ILSVRC2015_CLS-LOC.tar.gz
+  fi
+
+  if [ ! -d ILSVRC2015 ]; then
+    tar -zxvf ILSVRC2015_CLS-LOC.tar.gz
+  fi
+
+  cd "$ROOT_DIR"
+  python compressed_cryptonetes/data/make_miniImagenet_json.py --dataset_dir "$DATASET_DIR"
+  cd "$DATASET_DIR"
 fi
 
+# --------------------------------------------------
 # ImageNet-1K
-#  train/
-#  ├── n01440764
-#  │   ├── n01440764_10026.JPEG
-#  │   ├── n01440764_10027.JPEG
-#  │   ├── ......
-#  ├── ......
-#  val/
-#  ├── n01440764
-#  │   ├── ILSVRC2012_val_00000293.JPEG
-#  │   ├── ILSVRC2012_val_00002138.JPEG
-#  │   ├── ......
-#  ├── ......
-if [ "${parameterC}" == Y ]; then
-  echo -e "\n"
-  echo "Downloading and installing ImageNet-1K (ILSVRC2012)..."
-  # Download the data
-  # get ILSVRC2012_img_val.tar (about 6.3 GB). MD5: 29b22e2961454d5413ddabcf34fc5622
-  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
-  # get ILSVRC2012_img_train.tar (about 138 GB). MD5: 1d675b47d978889d74fa0da5fadfb00e
-  wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
+# --------------------------------------------------
+if [ "$parameterC" == "Y" ]; then
+  echo "ImageNet-1K"
 
-  # Extract the training data
-  mkdir ImageNet
+  mkdir -p ImageNet
   cd ImageNet
-  mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
-  tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
-  find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
-  cd ..
 
-  # Extract the validation data and move images to subfolders:
-  mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
-  wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+  mkdir -p train val
+
+  if [ ! -f train/ILSVRC2012_img_train.tar ]; then
+    wget -O train/ILSVRC2012_img_train.tar \
+      https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
+  else
+    echo "Train tar already exists"
+  fi
+
+  if [ ! -f val/ILSVRC2012_img_val.tar ]; then
+    wget -O val/ILSVRC2012_img_val.tar \
+      https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
+  else
+    echo "Val tar already exists"
+  fi
+
+  cd train
+  if [ -z "$(ls -A . | grep -v tar)" ]; then
+    tar -xvf ILSVRC2012_img_train.tar
+    find . -name "*.tar" -exec sh -c 'mkdir -p "${0%.tar}" && tar -xf "$0" -C "${0%.tar}" && rm "$0"' {} \;
+  else
+    echo "Train already extracted"
+  fi
+
+  cd ../val
+  if [ -z "$(ls -A . | grep -v tar)" ]; then
+    tar -xvf ILSVRC2012_img_val.tar
+    wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+  else
+    echo "Val already prepared"
+  fi
 fi
+
+echo "All requested datasets processed safely."
